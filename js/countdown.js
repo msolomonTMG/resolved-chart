64e404edecbd230014a75b75bc805387e7038945
Thrillist Tech <thrillisttech@deployserver.thrillist>
2017-03-30 13:04:14 -0400

    update countdown to 4pm

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 30, 2017 11:00:00").getTime();
+var countDownDate = new Date("Mar 30, 2017 16:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
